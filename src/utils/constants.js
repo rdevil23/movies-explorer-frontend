d81319d15aaf79d5981df596665cc50e1126925f
Dmitry <rdevil1337@gmail.com>
2024-05-08 23:16:36 +0500

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,2 @@
-// export const mainUrl = 'http://localhost:3000';
-export const mainUrl = 'https://movies-explorer-api-lyart.vercel.app/';
+export const mainUrl = 'https://movies-explorer-api-lyart.vercel.app';
 export const moviesUrl = 'https://api.nomoreparties.co/beatfilm-movies';
